6ff1c7d72a6d9879be0d7c877fd519e1024ab9f9
YasaminAlizadeh <itsYasaminAlizadeh@gmail.com>
2023-03-29 17:34:19 +0430

    feat: added option cards to page dynamically

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,24 @@ const states = [
 
 let currentState = 0;
 
+const optionsContainer = document.querySelector(".options");
+const setOptions = () => {
+  states.map((state) => {
+    const optionCard = document.createElement("div");
+    optionCard.classList.add("option");
+
+    optionCard.id = `option-${state.label}`;
+    optionCard.style.backgroundImage = `url('${state.image}')`;
+
+    optionCard.onclick = () => {
+      [...options].map((option) => option.classList.add("option--disabled"));
+
+    };
+
+    optionsContainer.appendChild(optionCard);
+    options = document.querySelectorAll(".option");
+  });
+};
+
+setOptions();
+
