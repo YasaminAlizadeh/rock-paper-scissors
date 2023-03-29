10b3fa0f2f41548ac7b0810ae063857fc4f24ef1
YasaminAlizadeh <itsYasaminAlizadeh@gmail.com>
2023-03-29 17:31:36 +0430

    feat: added possible states

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const states = [
+  { label: "rock", image: "../assets/raised-fist.png" },
+  { label: "paper", image: "../assets/raised-hand.png" },
+  { label: "scissors", image: "../assets/victory-hand.png" },
+];
+
+let currentState = 0;
+
